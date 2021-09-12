e107882865e422d74797c66e50ba0f94cb2b4d58
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-09-12 14:09:58 +0200

    initial commit

---
+++ src/main/java/pl/jaczewski/sklepdemo/SklepDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.jaczewski.sklepdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepDemoApplication.class, args);
+	}
+
+}
